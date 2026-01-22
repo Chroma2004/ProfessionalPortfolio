e4878f1068b3addee00b89e1a74cb09f691d1e3f
Chroma2004 <Chromaa2004@gmail.com>
2026-01-23 01:28:59 +0800

    new

--- src/Works.jsx
+++ src/Works.jsx
@@ -10,7 +10,7 @@ import Port1 from './assets/Port1.png';
 import Port2 from './assets/Port2.png';
 import Port3 from './assets/Port3.png';
 import Port4 from './assets/Port4.png';
-import GA1 from './assets/GA1.png';
+import GA1 from './assets/GA1.PNG';
 import GA2 from './assets/GA2.png';
 import GA3 from './assets/GA3.png';
 import GA4 from './assets/GA4.png';
